0cb28a64c9e99974d251e5f86808ffc142745ed0
titokrm <60860619+titokrm@users.noreply.github.com>
2026-07-09 16:22:52 +0300

    Delete trade-blade directory

--- trade-blade/#sources/js/import/modules.js
+++
@@ -1,6 +0,0 @@
-import "%modules%/header/header";
-import "%modules%/statistics/statistics";
-import "%modules%/completed-transactions/completed-transactions";
-import "%modules%/tariffs/tariffs";
-import "%modules%/faq/faq";
-import "%modules%/title-effect/title-effect";
